5f19106b888a23922e9011caf0db071518865597
Oscar <55315@etu.he2b.be>
2021-04-17 22:40:16 +0200

    write head and tails in files

--- hash_chain.h
+++ hash_chain.h
@@ -1,15 +1,22 @@
 #ifndef HASH_CHAIN_H
 #define HASH_CHAIN_H
 #include <string>
+#include <map>
 #define CHAIN_LENGTH 1000
+
+namespace rainbow {
 class Hash_Chain{
-    std::string head ;
-    std::string tail ;
+    std::string head_ ;
+    std::string tail_ ;
     void generateChain();
+    std::string generate_passwd(int length);
     std::string reduction_function(int lenghtOfPasswd,int nbOfReduction, std::string &hash);
     public:
+    std::string head() ;
+    std::string tail() ;
     Hash_Chain(int lenght);
     std::string to_string();
 };
+}
 
 #endif // HASh_CHAIN_H
