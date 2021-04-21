c4960683d187dcdb42e74d05f658120a683e8f6f
Oscar <55315@etu.he2b.be>
2021-04-21 11:27:49 +0200

    format

--- gen-passwd.cpp
+++ gen-passwd.cpp
@@ -9,21 +9,43 @@
 #include <future>
 #include <string>
 
-int make_addition(int i, int j)
- {
-     return i + j;
- }
+void sortFile(const std::string &of_pwd)
+{
+	std::vector<std::string> fileLines;
+	std::string line;
+	std::ifstream myfile(of_pwd);
+
+	//stack overflow example
+	if (!myfile) //test the file
+	{
+		std::cout << "Unable to open the file" << std::endl;
+	}
+
+	while (std::getline(myfile, line))
+	{
+		fileLines.push_back(line);
+		//cout << line << '\n';
+	}
+
+	sort(fileLines.begin(), fileLines.end()); //sorting string vector
+	std::ofstream newfile(of_pwd);
+	for (std::string &s : fileLines)
+	{
+
+		newfile << s << std::endl;
+	};
+}
 
 int main(int argc, char *argv[])
 {
-	if(argc != 5)
+	if (argc != 5)
 	{
 		std::cerr << "Usage : \"gen-passwd c mc MC of_pwd of_hash\", where" << std::endl
-			<< "- c is the size of rainbow table in gb ," << std::endl
-			<< "- mc is the minimum number of chars allowed in an alphanumeric password," << std::endl
-			<< "- MC is the maximum number of chars allowed in an alphanumeric password," << std::endl
-			<< "- of_pwd is the name of the output file where the passwords will be stored" << std::endl
-			<< "- of_hash is the name of the output file where the sha-256 hashes of the passwords will be stored" << std::endl;
+				  << "- c is the size of rainbow table in gb ," << std::endl
+				  << "- mc is the minimum number of chars allowed in an alphanumeric password," << std::endl
+				  << "- MC is the maximum number of chars allowed in an alphanumeric password," << std::endl
+				  << "- of_pwd is the name of the output file where the passwords will be stored" << std::endl
+				  << "- of_hash is the name of the output file where the sha-256 hashes of the passwords will be stored" << std::endl;
 		return 1;
 	}
 	//change in size of rainbow table .
@@ -32,50 +54,22 @@ int main(int argc, char *argv[])
 	int mc = std::stoi(argv[2]);
 	int MC = std::stoi(argv[3]);
 
- unsigned int n = std::thread::hardware_concurrency();
+	unsigned int n = std::thread::hardware_concurrency();
 
 	ThreadPool t(n); //a pool of 8 threads
 
-    std::vector<std::future<void>> v; 
-    std::cout << n << " concurrent threads are supported.\n";
+	std::vector<std::future<void>> v;
+	std::cout << n << " concurrent threads are supported.\n";
 
-    for(int i = 0; i < n; i++){
-        v.push_back(t.enqueue(rainbow::mass_generate, c, mc, MC, argv[4])); //I could push any function here, not only (int)(*f)(int,int)
-
+	for (int i = 0; i < n; i++)
+	{
+		v.push_back(t.enqueue(rainbow::mass_generate, c, mc, MC, argv[4])); //I could push any function here, not only (int)(*f)(int,int)
 	}
 
-	 std::vector<std::string> fileLines;
-	    std::string line;
-		std::ifstream myfile(argv[4]);
-
-    //stack overflow example
-    if (!myfile) //test the file
-    {
-        std::cout << "Unable to open the file" << std::endl;
-        return 0;
-    }
-
-		for (auto &&future : v)
+	for (auto &&future : v)
 	{
 		future.wait();
 	}
 
-    while (std::getline(myfile, line))
-    {
-        fileLines.push_back(line);
-        //cout << line << '\n';
-    }
-
-    sort(fileLines.begin(), fileLines.end()); //sorting string vector
-	std::ofstream newfile (argv[4]);
-    for (std::string &s : fileLines)
-    {
-        
-        newfile << s << std::endl;
-    };
-
-
-	
-
-	
+	sortFile(argv[4]);
 }
